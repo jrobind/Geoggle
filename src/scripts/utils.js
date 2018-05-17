4d5f4003032b2e65df30bfd6fefd8cd636277c31
James Robinson <jrobind@outlook.com>
2018-05-17 11:29:47 +0100

    edit shuffle() comment

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,6 +1,6 @@
 export default {
     shuffle(array) {
-        // shuffle array in place and splice and format population number if necessary
+        // shuffle countries array in place 
         for (let i = array.length -1; i > 0; i--) {
             const random = Math.floor(Math.random() * (i + 1));
             [array[i], array[random]] = [array[random], array[i]];
